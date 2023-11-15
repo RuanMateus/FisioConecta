adc0334d1aad1850ebd7908603070377468fd435
gihr0cha <giovana.rocha@estudante.ifb.edu.br>
2023-11-15 16:58:20 -0300

    tentativa firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'pages/createAccount_page.dart';
@@ -8,7 +9,9 @@ import 'pages/historico_page.dart';
 import 'pages/login_page.dart';
 import 'pages/registerPacients_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -35,7 +38,9 @@ final GoRouter _router = GoRouter(
         GoRoute(
           path: 'pacientes',
           builder: (BuildContext context, GoRouterState state) {
-            return const PacientePage(pacientes: [],);
+            return const PacientePage(
+              pacientes: [],
+            );
           },
         ),
         GoRoute(
@@ -69,3 +74,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(routerConfig: _router, title: 'My App');
   }
 }
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const FirebaseOptions web = FirebaseOptions(
+    apiKey: "AIzaSyDdzjOFXsNVH31sKjaZLH8aWo1QCiEX1WI",
+    authDomain: "fisioconecta-b9fcf.firebaseapp.com",
+    projectId: "fisioconecta-b9fcf",
+    storageBucket: "fisioconecta-b9fcf.appspot.com",
+    messagingSenderId: "521264665454",
+    appId: "1:521264665454:web:3d8531b1b608373550a77f",
+    measurementId: "G-J2Z219LMQS");
